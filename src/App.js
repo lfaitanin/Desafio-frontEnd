8894a9f3f0546e96f050f8bb7d436777629437c4
Luiz Faitanin <lfaitanin@outlook.com>
2020-02-29 18:09:18 -0300

    Inicial commit, finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios  from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class Planets extends Component{   
+  state = {
+    planets: [],
+    indice: 1
+  }
+  
+  nextPage() {
+    axios.get(`https://swapi.co/api/planets/${this.state.indice}/`)
+        .then(res => {
+          const planets = res.data;
+          this.setState({
+            planets: planets,
+            indice: ++this.state.indice 
+          })
+        })
+    }
+    previewPage() {
+      axios.get(`https://swapi.co/api/planets/${this.state.indice}/`)
+          .then(res => {
+            if(this.state.indice === 0) {
+              alert("last page bro")
+              return;
+            }
+            const planets = res.data;
+            this.setState({
+              planets: planets,
+              indice: --this.state.indice 
+            })
+          })
+      }
+    onClick = (indice) => {
+      this.setState({ indice : this.state.indice});
+      if(indice.target.className === 'next') {          
+        this.nextPage()
+      }
+      if(indice.target.className === 'preview') {
+          this.previewPage()
+      }
+   }
 
-export default App;
+    render() {
+      const mystyle = {
+        textAlign: "center",
+        padding: "200px",
+        backgroundColor: "#bcbfa2"
+      };
+      const quadro = {
+        border: "3px solid black",
+        borderStyle: "inset",
+        width: "30%",
+        margin: "0 auto"
+      }
+        const planeta = this.state.planets;
+      return (
+        <div  style={mystyle}>
+            <div style={quadro}>
+             <h1>Nome: { planeta.name}</h1>
+                <h3>Clima: { planeta.climate}
+                <br/>
+                c
+                    Terrain: { planeta.terrain}
+                <br/>
+                    Populacao: { planeta.population}
+                </h3>
+        
+         <button onClick={this.onClick} className="preview">preview</button>
+         <button onClick={this.onClick}className="next">next</button>
+
+         </div>
+        </div>
+      )
+      
+    }
+    
+  }
+
